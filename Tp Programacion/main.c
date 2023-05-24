445cb5c4c51e356b85f761ed34152663266ef389
milir99 <109126977+milir99@users.noreply.github.com>
2023-05-24 16:08:15 -0300

    añadi switch

--- Tp Programacion/main.c
+++ Tp Programacion/main.c
@@ -3,6 +3,52 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+int eleccion;
+do{
+    printf("\nIngrese la opcion que desea realizar \n");
+    printf(" 0.Terminar el programa\n");
+    printf(" 1.\n");
+    printf(" 2.\n");
+    printf(" 3.\n");
+    printf(" 4. \n");
+    printf(" 5.\n");
+    printf(" 6.\n");
+    printf(" 7.\n");
+    printf(" 8.\n");
+    printf(" 9.\n");
+    printf(" 10.\n");
+
+
+
+
+    fflush(stdin);
+    scanf("%i",&eleccion);
+    switch(eleccion)
+    {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;     
+        case 9:
+            break; 
+        case 10:
+            break;               
+}
+    } while (eleccion != 0);
 }
